8ea98158a3bb441844fc5709446e3aa2445d103f
AdrianoR85 <adrianorosa085@gmail.com>
2023-10-31 12:22:58 -0300

    feat: Added yup library

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { StatusCodes } from 'http-status-codes'
+import { CitiesController } from "./../controllers";
+
 const router = Router();
 
 router.get("/", (req, res) => {
   return res.send("Hello");
 });
 
-router.post("/test", (req, res) => {
-  return res.json(req.body);
-});
+router.post("/cities", CitiesController.create);
 
 export { router }
